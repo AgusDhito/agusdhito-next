fb188ffabcee0a1227d37a532cbed2f8f54afd36
Agustinus Ardhito <aardhito212@gmail.com>
2025-03-13 18:41:12 +0700

    working experiences

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ResponsiveAppBar from "@/app/ui/NavBar";
+import { Footer } from "@/app/ui/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,8 @@ export default function RootLayout({
       >
         <ResponsiveAppBar />
         {children}
+
+        <Footer/>
       </body>
     </html>
   );
