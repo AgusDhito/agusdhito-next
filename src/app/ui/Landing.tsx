faff4e92f28b823d6d42c8952d7b570482c13c12
Agustinus Ardhito <aardhito212@gmail.com>
2025-04-28 17:21:46 +0700

    more layouting

--- src/app/ui/Landing.tsx
+++ src/app/ui/Landing.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import Image from "next/image"
-import { Divider, Grid2, Typography, Box, Container, Button } from "@mui/material"
-import landingLogo from "./public/landing.jpg"
+import { Divider, Grid2, Typography, Box, Container, Button, List, ListItem, IconButton, Collapse } from "@mui/material"
+import landingLogo from "./public/landing-new.jpg"
 import { useEffect, useState, useRef } from "react";
 import { Rect, useRect } from "react-use-rect";
 import { relative } from "path";
@@ -14,16 +14,20 @@ import {
     AlarmOutlined,
     GitHub,
     Mail,
-    LinkedIn
+    LinkedIn,
+    KeyboardArrowDown,
+    KeyboardArrowUp
 } from "@mui/icons-material"
 import { WorkingExperiences } from "@/app/data/data";
 import { ExperienceBox } from "@/app/ui/ExperienceBox";
+import zIndex from "@mui/material/styles/zIndex";
 
 
 function Landing() {
     const mock = ["Engineering Manager", "Backend Engineer", "Frontend Engineer", "Guitarist", "Music Producer"];
     const slidePictures = ["golang.png", "ruby-on-rails.png", "java.png", "postgresql.png", "redis.png", "react.png", "typescript.png", "docker.png", "kubernetes.png", "gcp.png"];
 
+    const [detailsOpen, setDetailsOpen] = useState(false)
     const SlideText = ({ source }: { source: string[] }) => {
         const [currentItemIndex, setCurrentItemIndex] = useState(0);
         const [rect, setRect] = useState<Rect | null>(null);
@@ -55,12 +59,15 @@ function Landing() {
                     className="w-full"
                     ref={currentItemIndex === index ? rectRef : null}
                     style={{
-                        position: "absolute",
-                        top: (rect?.height ?? 0) * 2,
-                        transform: `translateY(${
-                        currentItemIndex === index ? `-${(rect?.height ?? 0) * 2}px` : 0
-                        })`,
-                        transition: "all 1s ease-in-out",
+                      position: "absolute",
+                      // top: (rect?.height ?? 0) * 2,
+                      left: (rect?.width?? 0) * 2,
+                      // transform: `translateY(${
+                      // currentItemIndex === index ? `-${(rect?.height ?? 0) * 2}px` : 0
+                      transform: `translateX(${
+                      currentItemIndex === index ? `-${(rect?.width ?? 0) * 2}px` : 0
+                      })`,
+                      transition: "all 1s ease-in-out",
                     }}
                     >
                     {text}
@@ -125,95 +132,160 @@ function Landing() {
 
     return (
         <>
-            <Box
+            <Container
+            maxWidth="xl"
             style={{
-                height: "90vh",
-                padding: "0px",
-                position: "relative",
+              height: "90vh",
+              // padding: "0px",
+              position: "relative",
+              display: "inline-flex"
             }}
             >
-                <Image
-                src={landingLogo}
-                alt="Landing page"
-                // width="80%"
-                fill
-                className="opacity-40"
-                />
+              <Box
+              style={{
+                backgroundColor: "lightblue",
+                display: "inline-flex",
+                zIndex: 1
+              }}
+              >
+                <Box
+                style={{
+                  width: "50%",
+                  height: "100%",
+                  padding: "8px",
+                  margin: "8px",
+                  // backgroundColor: "black"
+                }}
+                >
+                  <Box
+                  style={{
+                    height: "90%",
+                    width: "90%",
+                    position: "relative"
+                  }}>
+                    <Image
+                    src={landingLogo}
+                    alt="Landing page"
+                    // width="80%"
+                    fill
+                    // className="opacity-40"
+                    />
+                  </Box>
+
+                  <Box
+                  style={{
+                    position: "relative",
+                    bottom: "85%",
+                    left: "10%",
+                    height: "90%",
+                    width: "90%",
+                    backgroundColor: "white",
+                    zIndex: -1
+                  }}>
+
+                  </Box>
+                </Box>
 
                 <Box
                 sx={{
-                    height: "100%",
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "right",
+                    margin: "4px",
+                    textAlign: "right",
+                    width: "50%",
+                    padding: "32px",
                 }}
                 >
-                    <Box
+                    <Typography variant="h3">
+                        Agustinus Ardhito Vedoputro
+                    </Typography>
+                    <br/>
+                    <Typography className="pt-4">
+                        Hello everyone, nice to meet you! I'm a
+                    </Typography>
+                    <br/>
+                    <Typography variant="h5">
+                        <SlideText source={mock}/>
+                    </Typography>
+
+                    <Typography
                     sx={{
-                        margin: "4px",
-                        textAlign: "right",
-                        width: "50%",
-                        padding: "32px",
-                        backgroundColor: "white"
+                      fontStyle: "italic"
                     }}
                     >
-                        <Typography variant="h3">
-                            Agustinus Ardhito Vedoputro
-                        </Typography>
-                        <br/>
-                        <Typography variant="h5">
-                            <SlideText source={mock}/>
-                        </Typography>
+                      (See full of what I do) :
+                      <IconButton
+                      onClick={() => {setDetailsOpen(!detailsOpen)}}
+                      >
+                        {
+                          detailsOpen ? (<KeyboardArrowUp/>) : (<KeyboardArrowDown/>)
+                        }
+                      </IconButton>
+                    </Typography>
 
-                        <Typography className="pt-4">
-                            I'm an experienced Engineering Manager & Software Engineer with full experience expanding technology with enterprise-based standards.
-                            Nice to meet you!
-                        </Typography>
+                    <Collapse in={detailsOpen}>
+                      <List>
+                        <ListItem>
+                          I'm a Software Engineering Manager that likes to coach & empower people, and experienced handling teams & platforms with millions of traffic.
+                        </ListItem>
+                        <ListItem>
+                          An experienced software engineer that likes to tinker from the database to application layer, and experienced in handling industry-level platform with millions of users.
+                        </ListItem>
+                        <ListItem>
+                          A guitarist & music producer that like to experiment & combining multiple & cross music genres.
+                        </ListItem>
+                        <ListItem>
+                          An energetic traveler.
+                        </ListItem>
+                      </List>
+                    </Collapse>
 
-                        <Box
-                            sx={{
-                                display: "flex",
-                                justifyContent: "right",
-                            }}
+
+                    <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "right",
+                    }}
+                    >
+                        <div className="size-8">
+                            <Link href="https://www.linkedin.com/in/agustinus-ardhito">
+                                <Button>
+                                    <LinkedIn></LinkedIn>
+                                </Button>
+                            </Link>
+                        </div>
+                        <div className="size-8 mx-2">
+                            <Link
+                            href="mailto:agusdhito@gmail.com"
                             >
-                                <div className="size-8">
-                                    <Link href="https://www.linkedin.com/in/agustinus-ardhito">
-                                        <Button>
-                                            <LinkedIn></LinkedIn>
-                                        </Button>
-                                    </Link>
-                                </div>
-                                <div className="size-8 mx-2">
-                                    <Link
-                                    href="mailto:agusdhito@gmail.com"
-                                    >
-                                      <Button>
-                                          <Mail></Mail>
-                                      </Button>
-                                    </Link>
-                                </div>
+                              <Button>
+                                  <Mail></Mail>
+                              </Button>
+                            </Link>
+                        </div>
 
-                                <div className="size-8">
-                                    <Link href="https://github.com/AgusDhito">
-                                        <Button
-                                        style={{width: "100%"}}
-                                        >
-                                            <GitHub></GitHub>
-                                        </Button>
-                                    </Link>
-                                </div>
-                            </Box>
+                        <div className="size-8">
+                            <Link href="https://github.com/AgusDhito">
+                                <Button
+                                style={{width: "100%"}}
+                                >
+                                    <GitHub></GitHub>
+                                </Button>
+                            </Link>
+                        </div>
                     </Box>
                 </Box>
+              </Box>
 
-            </Box>
+            </Container>
 
             <Container
             maxWidth="xl"
             >
                 {/*  Services I offer */}
                 <div
-                className="m-16 text-center"
+                className="p-8 text-center"
+                style={{
+                  backgroundColor: "#a55742"
+                }}
                 >
                     <Typography variant="h5">
                         Expertises & Services
